f86c5eed2159a8f0886e6131ce94dc8f37116879
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2023-03-04 19:51:50 +0100

    feat: Initial commit

---
+++ kq_interior_light/client/client.lua
@@ -0,0 +1,46 @@
+Citizen.CreateThread(function()
+    local input = Config.keybinds.toggle.input
+    local pressedAt = nil
+    
+    while true do
+        local sleep = 3000
+        
+        local playerPed = PlayerPedId()
+        if IsPedInAnyVehicle(playerPed) then
+            local veh = GetVehiclePedIsIn(playerPed)
+            
+            -- Whether or not the ped is the driver
+            if GetPedInVehicleSeat(veh, -1) == playerPed then
+                sleep = 50
+                
+                if IsControlPressed(0, input) or IsDisabledControlPressed(0, input) then
+                    if pressedAt == nil then
+                        pressedAt = GetGameTimer()
+                    elseif pressedAt + 100 < GetGameTimer() then
+                        -- This will disable the headlight control when the keybind is pressed down. By having the 100ms
+                        -- of threshold the headlight control will still function normally when clicked (not pressed)
+                        sleep = 1
+                        DisableControlAction(0, input)
+                    end
+                    
+                    if pressedAt + Config.keybinds.toggle.holdDuration < GetGameTimer() then
+                        SetVehicleInteriorlight(veh, not IsVehicleInteriorLightOn(veh))
+                        pressedAt = nil
+                        
+                        Citizen.CreateThread(function()
+                            local disableUntil = GetGameTimer() + 100
+                            while GetGameTimer() < disableUntil do
+                                Citizen.Wait(1)
+                                DisableControlAction(0, input)
+                            end
+                        end)
+                    end
+                else
+                    pressedAt = nil
+                end
+            end
+        end
+        
+        Citizen.Wait(sleep)
+    end
+end)
---
+++ kq_interior_light/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+-- https://docs.fivem.net/docs/game-references/controls/
+-- Use the input index for the "input" value
+Config.keybinds = {
+    toggle = {
+        label = 'H',
+        input = 74,
+        holdDuration = 1000,
+    }
+}
